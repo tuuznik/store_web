e444eb8628b0d3617c18c47915b65d45280300b2
tuuznik <tuznikagnieszka@gmail.com>
2021-04-11 19:50:54 +0200

    Initial project added

---
+++ src/pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, 'home.html', {})
+
+
+def contact_view(request, *args, **kwargs):
+    return render(request, 'contact.html', {})
+
+
+def about_view(request, *args, **kwargs):
+    return render(request, 'about.html', {})
+
+
+def social_view(request, *args, **kwargs):
+    return render(request, 'social.html', {})
